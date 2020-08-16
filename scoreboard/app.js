ffd62670e814638a7f875fbfdd10b8ab98010fa0
Daniel Clarke <daniel.clarke@bluebaytravel.co.uk>
2020-08-16 21:59:26 +0100

    create a counter component and user in in player component

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -7,8 +7,29 @@ const Header = () => {
     );
 }
 
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                Daniel Clarke
+            </span>
+            <Counter />
+        </div>
+    );
+}
+
+const Counter = () => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">35</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+}
+
 
 ReactDOM.render(
-    <Header />,
+    <Player />,
     document.getElementById('root')
 );
