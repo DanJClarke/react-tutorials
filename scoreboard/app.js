d412fa3e2465ae5056f1fb8a4e63323fb0305e15
Daniel Clarke <daniel.clarke@bluebaytravel.co.uk>
2020-08-17 17:18:47 +0100

    Add Props to counter and player components

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -7,22 +7,22 @@ const Header = (props) => {
     );
 }
 
-const Player = () => {
+const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
-                Daniel Clarke
+                { props.name }
             </span>
-            <Counter />
+            <Counter score={ props.score } />
         </div>
     );
 }
 
-const Counter = () => {
+const Counter = (props) => {
     return (
         <div className="counter">
             <button className="counter-action decrement"> - </button>
-            <span className="counter-score">35</span>
+            <span className="counter-score">{ props.score }</span>
             <button className="counter-action increment"> + </button>
         </div>
     );
@@ -36,7 +36,7 @@ const App = () => {
                 totalPlayers={ 1 }
             />
             {/* Players List */}
-            <Player />
+            <Player name="Daniel Clarke" score="34" />
         </div>
     );
 }
