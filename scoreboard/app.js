c4338e61edb320fd17dec4c1384f645b45c36121
Daniel Clarke <daniel.clarke@bluebaytravel.co.uk>
2020-08-17 23:35:06 +0100

    reference state directly

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -42,11 +42,9 @@ const Player = (props) => {
 }
 
 class Counter extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            score: 0
-        }
+
+    state = {
+        score: 0
     }
 
     render() {
