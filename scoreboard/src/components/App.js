425ece1622cc5eac2bad589cd5f93967d5cb2e01
Daniel Clarke <daniel.clarke@bluebaytravel.co.uk>
2020-08-24 23:34:33 +0100

    Building the Statistics Component using reduce

--- scoreboard/src/components/App.js
+++ scoreboard/src/components/App.js
@@ -44,7 +44,7 @@ class App extends Component {
           <div className="scoreboard">
               <Header
                   title="My Scoreboard"
-                  totalPlayers={ this.state.players.length }
+                  players={ this.state.players }
               />
               { this.state.players.map( (player, index) =>
                   <Player
