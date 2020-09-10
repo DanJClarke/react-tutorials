bd9c14624ec41b395aeeb5dc275e1d7c80a62450
Daniel Clarke <daniel.clarke@bluebaytravel.co.uk>
2020-09-10 17:45:08 +0100

    Connect the Scoreboard Container to Redux

--- scoreboard/src/containers/Scoreboard.js
+++ scoreboard/src/containers/Scoreboard.js
@@ -1,4 +1,8 @@
 import React, { Component } from  '../../node_modules/react';
+import PropTypes from '../../node_modules/prop-types';
+import { bindActionCreators } from  '../../node_modules/redux';
+import * as PlayerActionCreators from '../actions/player';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 import Player from '../components/Player';
 import AddPlayerForm from '../components/AddPlayerForm';
@@ -15,29 +19,43 @@ class Scoreboard extends Component {
 
   render() {
     //const highScore = this.getHighScore();
+    const { dispatch, players } = this.props;
+    const handlerRemovePlayer = bindActionCreators(PlayerActionCreators.removeplayer, dispatch);
+    const handleScoreChange = bindActionCreators(PlayerActionCreators.updatePlayerScore, dispatch);
+    const handleAddPlayer = bindActionCreators(PlayerActionCreators.addplayer, dispatch);
 
       return (
           <div className="scoreboard">
               <Header
                   title="My Scoreboard"
-                  players={ this.state.players }
+                  players={ players }
               />
-              { this.state.players.map( (player, index) =>
+              { players.map( (player, index) =>
                   <Player
                       name={ player.name }
                       score={ player.score }
                       id={ player.id }
                       index = { index }
-                      changeScore={ this.handleScoreChange }
+                      changeScore={ handleScoreChange }
                       key={ player.id.toString() }
-                      removePlayer={ this.handlerRemovePlayer }
+                      removePlayer={ handlerRemovePlayer }
                      // isHighScore={highScore === player.score}
                   />
               ) }
-              <AddPlayerForm addPlayer={ this.handleAddPlayer}/>
+              <AddPlayerForm addPlayer={ handleAddPlayer }/>
           </div>
       );
   }
 }
 
-export default Scoreboard;
+Scoreboard.propTypes = {
+    players: PropTypes.array.isRequired
+};
+
+const mapsStateToProps = state => (
+    {
+        players: state
+    }
+);
+
+export default connect(mapsStateToProps)(Scoreboard);
