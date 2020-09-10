7f59a7b7c1cbc10f0708850da350cb8a8307b666
Daniel Clarke <daniel.clarke@bluebaytravel.co.uk>
2020-09-10 17:33:27 +0100

    remove state and methods from Scoreboard

--- scoreboard/src/containers/Scoreboard.js
+++ scoreboard/src/containers/Scoreboard.js
@@ -3,60 +3,6 @@ import Header from '../components/Header';
 import Player from '../components/Player';
 import AddPlayerForm from '../components/AddPlayerForm';
 class Scoreboard extends Component {
-    state = {
-        players: [
-            {
-                name: "Daniel",
-                score: 0,
-                id: 1
-            },
-            {
-                name: "John",
-                score: 0,
-                id: 2
-            },
-            {
-                name: "Fred",
-                score: 0,
-                id: 3
-            },
-            {
-                name: "Joe",
-                score: 0,
-                id: 4
-            }
-        ]
-    }
-
-    prevPlayerId = 4
-
-    handlerRemovePlayer = (id) => {
-        this.setState(prevState => ({
-            players: prevState.players.filter(p => p.id !== id)
-        }));
-    }
-
-    handleScoreChange = (index, delta) => {
-        this.setState(prevState => ({
-            score: prevState.players[index].score += delta
-        }));
-    }
-
-    handleAddPlayer = (name) => {
-        this.setState(preveState => {
-            return {
-                players: [
-                    ...preveState.players,
-                    {
-                        name,
-                        score: 0,
-                        id: this.prevPlayerId += 1
-                    }
-                ]
-            };
-        });
-    }
-
     getHighScore = () => {
         const scores = this.state.players.map( p => p.score );
         const highScore = Math.max(...scores);
