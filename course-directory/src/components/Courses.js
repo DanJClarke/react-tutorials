08ed998b136683bfc3cb91b761dc242bda64b966
Daniel Clarke <daniel.clarke@bluebaytravel.co.uk>
2020-09-21 23:30:16 +0100

    fix match

--- course-directory/src/components/Courses.js
+++ course-directory/src/components/Courses.js
@@ -14,15 +14,15 @@ const Courses = ({match}) => (
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <li><NavLink to={ `${match.url}/html` }>HTML</NavLink></li>
-        <li><NavLink to={ `${match.url}/css` }>CSS</NavLink></li>
+        <li><NavLink to={`${match.url}/html`}>HTML</NavLink></li>
+        <li><NavLink to={`${match.url}/css`}>CSS</NavLink></li>
         <li><NavLink to={`${match.url}/javascript`}>JavaScript</NavLink></li>
       </ul>
     </div>
-    <Route exact path={ match.path } render={ () => <Redirect to="/courses/html"/> }/>
-    <Route path={ `${match.path}/html `}component={ HTML }/>
-    <Route path={ `${match.path}/css `}component={ CSS }/>
-    <Route path={ `${match.path}/javascript `} component={ JavaScript }/>
+    <Route exact path={match.path} render={ () => <Redirect to={`${match.path}/html`}/> }/>
+    <Route path={`${match.path}/html`} component={ HTML }/>
+    <Route path={`${match.path}/css`} component={ CSS }/>
+    <Route path={`${match.path}/javascript`} component={ JavaScript }/>
   </div>
 );
 
