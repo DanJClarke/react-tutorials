417a7907ba4e0e8c4fe0735cbf9dced00ab3efc0
Daniel Clarke <daniel.clarke@bluebaytravel.co.uk>
2021-01-25 23:35:45 +0000

    Fetching Data with the Fetch API

--- gif-search_no-create-react-app/src/App.js
+++ gif-search_no-create-react-app/src/App.js
@@ -7,9 +7,24 @@ export default class App extends Component {
   
   constructor() {
     super();
+    this.state = {
+      gifs: []
+    };
   } 
 
-  render() { 
+  componentDidMount() {
+    fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+      .then(response => response.json())
+      .then(responseDate =>{
+        this.setState({ gifs: responseDate.data });
+      })
+      .catch(error => {
+        console.log('Error fetching or parsing data ', error);
+      });
+  }
+
+  render() {
+    console.log(this.state.gifs);
     return (
       <div>
         <div className="main-header">
