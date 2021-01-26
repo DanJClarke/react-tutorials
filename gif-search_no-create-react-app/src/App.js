eb1f565f00a2c243d472828ac45a87629b625a32
Daniel Clarke <daniel.clarke@bluebaytravel.co.uk>
2021-01-26 00:12:13 +0000

    Display the data

--- gif-search_no-create-react-app/src/App.js
+++ gif-search_no-create-react-app/src/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
   } 
 
   componentDidMount() {
-    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=<API-KEY-GOES-HERE>')
       .then(response =>{
         this.setState({ gifs: response.data.data });
       })
@@ -34,7 +34,7 @@ export default class App extends Component {
           </div>   
         </div>    
         <div className="main-content">
-          <GifList />
+          <GifList data={ this.state.gifs } />
         </div>
       </div>
     );
