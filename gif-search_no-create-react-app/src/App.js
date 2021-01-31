8e8e12b8e700ae710b46b566da3836f7c5a6e31e
Daniel Clarke <daniel.clarke@bluebaytravel.co.uk>
2021-01-31 16:47:33 +0000

    Starter files

--- gif-search_no-create-react-app/src/App.js
+++ gif-search_no-create-react-app/src/App.js
@@ -1,56 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react'
+import '../App.css';
 import axios from 'axios';
-import SearchForm from './Components/SearchForm';
-import GifList from './Components/GifList';
 
-export default class App extends Component {
-  
-  constructor() {
-    super();
-    this.state = {
-      gifs: [],
-      loading: true
-    };
-  } 
+import SearchForm from './SearchForm';
+import GifList from './GifList';
 
-  componentDidMount() {
-    this.performSearch();
-  }
+function App() {
 
-  performSearch = (query = 'cats') => {
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=v7UWjlHJAxm2K5S4NlSH1KqWeq2CxpZj`)
-      .then(response =>{
-        this.setState({
-          gifs: response.data.data,
-          loading: false
-        });
-      })
-      .catch(error => {
-        console.log('Error fetching or parsing data ', error);
-      });
-  }
-
-  render() {
-    console.log(this.state.gifs);
-    return (
-      <div>
-        <div className="main-header">
-          <div className="inner">
-            <h1 className="main-title">GifSearch</h1>
-            <SearchForm onSearch={ this.performSearch } />      
-          </div>   
-        </div>    
-        <div className="main-content">
-          {
-            (this.state.loading)
-            ?
-              <p>loading...</p>
-            :
-              <GifList data={ this.state.gifs } />
-          }
+  return (
+    <div>
+      <div className="main-header">
+        <div className="inner">
+          <h1 className="main-title">GifSearch</h1>
+          <SearchForm />
         </div>
       </div>
-    );
-  }
+      <div className="main-content">
+
+      </div>
+    </div>
+  );
 }
+
+export default App
